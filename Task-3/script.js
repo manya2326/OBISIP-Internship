81597a4a7eedbd37133da96ad4f8fbc330510024
manya2326 <132253494+manya2326@users.noreply.github.com>
2023-08-03 11:46:14 +0530

    initial files added

---
+++ Task-3/script.js
@@ -0,0 +1,42 @@
+function convertTemperature() {
+    const inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    const fromUnit = document.getElementById("fromUnit").value;
+    const toUnit = document.getElementById("toUnit").value;
+    
+    if (isNaN(inputTemp)) {
+      alert("Please enter a valid number for the temperature.");
+      return;
+    }
+    
+    let result;
+    if (fromUnit === "celsius") {
+      if (toUnit === "fahrenheit") {
+        result = (inputTemp * 9/5) + 32;
+      } else if (toUnit === "kelvin") {
+        result = inputTemp + 273.15;
+      } else {
+        result = inputTemp;
+      }
+    } else if (fromUnit === "fahrenheit") {
+      if (toUnit === "celsius") {
+        result = (inputTemp - 32) * 5/9;
+      } else if (toUnit === "kelvin") {
+        result = (inputTemp - 32) * 5/9 + 273.15;
+      } else {
+        result = inputTemp;
+      }
+    } else if (fromUnit === "kelvin") {
+      if (toUnit === "celsius") {
+        result = inputTemp - 273.15;
+      } else if (toUnit === "fahrenheit") {
+        result = (inputTemp - 273.15) * 9/5 + 32;
+      } else {
+        result = inputTemp;
+      }
+    }
+    
+    document.getElementById("result").textContent = `Converted temperature: ${result.toFixed(2)} ${toUnit}`;
+  }
+  
+  document.getElementById("convertBtn").addEventListener("click", convertTemperature);
+  
